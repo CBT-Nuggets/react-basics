6afc82b412912d5cb3e95d9708ca3164ff759f32
Shaun Wassell <shaun@trove.ai>
2021-07-21 17:42:44 -0400

    02 Inserting Variables into JSX

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import './App.css';
 
+const myProfileData = {
+	name: 'Shaun Wassell',
+	profilePicUrl: `${process.env.PUBLIC_URL}/my-profile-pic.png`,
+	age: 100,
+	bio: 'I like to program. I also like food.',
+	birthday: 'March 1',
+	interests: ['Programming', 'Data Science', 'Gardening', 'Foreign Languages'],
+};
+
 function App() {
 	return (
 		<div>
 			<h1>Friend Tracker</h1>
 			<div>
 				<img
-					src={process.env.PUBLIC_URL + '/my-profile-pic.png'}
-					alt="Shaun smiling"
+					src={myProfileData.profilePicUrl}
+					alt={`${myProfileData.name} smiling`}
 					height="200" />
 				<h2>My Profile</h2>
 				<h3>Name</h3>
-				<p>Shaun Wassell</p>
+				<p>{myProfileData.name}</p>
 				<h3>Age</h3>
-				<p>100</p>
+				<p>{myProfileData.age}</p>
 				<h3>Bio</h3>
-				<p>I like to program. I also like food.</p>
+				<p>{myProfileData.bio}</p>
 				<h3>Birthday</h3>
-				<p>March 1</p>
+				<p>{myProfileData.birthday}</p>
 				<h3>Interests</h3>
-				<ul>
-					<li>Programming</li>
-					<li>Data Science</li>
-					<li>Gardening</li>
-					<li>Foreign Languages</li>
-				</ul>
+				<p>{myProfileData.interests.join(', ')}</p>
 			</div>
 		</div>
 	);
