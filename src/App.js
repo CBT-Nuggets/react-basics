00aa0561db79991f0b36e4d52923bd15fd501509
Shaun Wassell <shaun@trove.ai>
2021-07-22 13:08:01 -0400

    12 Showing and hiding elements (part 2)

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ import { PersonCard } from './PersonCard';
 import styles from './App.module.css';
 
 export const App = () => {
-	const [showWelcomeMessage, setShouldShowWelcomeMessage] = useState(true);
+	// const [showWelcomeMessage, setShouldShowWelcomeMessage] = useState(true);
 
-	function toggleWelcomeMessage() {
-		setShouldShowWelcomeMessage(!showWelcomeMessage);
-	}
+	// function toggleWelcomeMessage() {
+	// 	setShouldShowWelcomeMessage(!showWelcomeMessage);
+	// }
 
 	return (
 		<div>
 			<h1>Friend Tracker</h1>
 			<div className={styles.contentContainer}>
-				{showWelcomeMessage && <WelcomeMessage name={myProfileData.name} />}
-				<button onClick={toggleWelcomeMessage}>Toggle Message</button>
+				{/* {showWelcomeMessage && <WelcomeMessage name={myProfileData.name} />} */}
+
+				{/* Other ways of doing it 
+					{showWelcomeMessage ? <WelcomeMessage name={myProfileData.name} /> : null}
+				*/}
+
+				{/* <button onClick={toggleWelcomeMessage}>
+						{showWelcomeMessage ? 'Hide Message' : 'Show Message'}
+					</button> */}
+				<WelcomeMessage name={myProfileData.name} />
 				<h2 className={styles.contentHeading}>My Profile</h2>
 				<ProfileInfo person={myProfileData} />
 				<h2 className={styles.contentHeading}>My Friends</h2>
