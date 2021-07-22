7f801b081570f7642357bdcf063547d6dbc5cbec
Shaun Wassell <shaun@trove.ai>
2021-07-22 13:09:32 -0400

    Cleanup old comments

--- src/App.js
+++ src/App.js
@@ -6,25 +6,10 @@ import { PersonCard } from './PersonCard';
 import styles from './App.module.css';
 
 export const App = () => {
-	// const [showWelcomeMessage, setShouldShowWelcomeMessage] = useState(true);
-
-	// function toggleWelcomeMessage() {
-	// 	setShouldShowWelcomeMessage(!showWelcomeMessage);
-	// }
-
 	return (
 		<div>
 			<h1>Friend Tracker</h1>
 			<div className={styles.contentContainer}>
-				{/* {showWelcomeMessage && <WelcomeMessage name={myProfileData.name} />} */}
-
-				{/* Other ways of doing it 
-					{showWelcomeMessage ? <WelcomeMessage name={myProfileData.name} /> : null}
-				*/}
-
-				{/* <button onClick={toggleWelcomeMessage}>
-						{showWelcomeMessage ? 'Hide Message' : 'Show Message'}
-					</button> */}
 				<WelcomeMessage name={myProfileData.name} />
 				<h2 className={styles.contentHeading}>My Profile</h2>
 				<ProfileInfo person={myProfileData} />
