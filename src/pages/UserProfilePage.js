edee82f3176db69491c0494041544d047dcc9956
Shaun Wassell <shaun@trove.ai>
2021-07-25 16:16:53 -0400

    31 Added editing ability to User Profile Page, using the same ProfileInfoForm component

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,11 +1,29 @@
-import { myProfileData } from '../data';
+import { useState } from 'react';
+import { myProfileData as startingInfo } from '../data';
 import { ProfileInfo } from '../components/ProfileInfo';
+import { PersonInfoForm } from '../components/PersonInfoForm';
 
 const UserProfilePage = () => {
+	const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem('userInfo')) || startingInfo);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const updateUserInfo = updatedInfo => {
+		setUserInfo(updatedInfo);
+		localStorage.setItem('userInfo', JSON.stringify(updatedInfo));
+		setIsEditing(false);
+	}
+
+	const actions = [{
+		actionName: 'Edit My Info',
+		handler: () => setIsEditing(true),
+	}];
+
 	return (
 		<>
 		<h2 className="content-heading">My Profile</h2>
-		<ProfileInfo person={myProfileData} />
+		{isEditing
+			? <PersonInfoForm person={userInfo} onSubmit={updateUserInfo} buttonText="Save Changes" />
+			: <ProfileInfo person={userInfo} actions={actions} />}
 		</>
 	);
 }
