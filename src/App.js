d53d785a46edc445d598dd6f4f84eae1ac003cff
Shaun Wassell <shaun@trove.ai>
2021-07-29 19:35:17 -0400

    Cleanup after profiling demo

--- src/App.js
+++ src/App.js
@@ -8,42 +8,29 @@ import { FriendDetailPage } from './pages/FriendDetailPage';
 import { FriendsPage } from './pages/FriendsPage';
 import { NewFriendPage } from './pages/NewFriendPage';
 import { UserProfilePage } from './pages/UserProfilePage';
-import { trackRender } from './performance/trackRender';
 import styles from './App.module.css';
 
 export const App = () => {
 	return (
 		<BrowserRouter>
-			<Profiler id="Navigation" onRender={trackRender}>
-				<NavBar />
-			</Profiler>
+			<NavBar />
 			<FriendsProvider>
 				<FavoritesProvider>
 					<div className={styles.contentContainer}>
 						<Route path="/" exact>
-							<Profiler id="Friends Page" onRender={trackRender}>
-								<FriendsPage />
-							</Profiler>
+							<FriendsPage />
 						</Route>
 						<Route path="/user-profile">
-							<Profiler id="User Profile Page" onRender={trackRender}>
-								<UserProfilePage />
-							</Profiler>
+							<UserProfilePage />
 						</Route>
 						<Route path="/friends/:friendId">
-							<Profiler id="Friend Detail Page" onRender={trackRender}>
-								<FriendDetailPage />
-							</Profiler>
+							<FriendDetailPage />
 						</Route>
 						<Route path="/edit-friend/:friendId">
-							<Profiler id="Edit Friend Page" onRender={trackRender}>
-								<EditFriendPage />
-							</Profiler>
+							<EditFriendPage />
 						</Route>
 						<Route path="/new-friend">
-							<Profiler id="New Friend Page" onRender={trackRender}>
-								<NewFriendPage />
-							</Profiler>
+							<NewFriendPage />
 						</Route>
 					</div>
 				</FavoritesProvider>
