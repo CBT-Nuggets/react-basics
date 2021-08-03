5046ec7123594d64528a7d16d21bc692ef6d3d21
Shaun Wassell <shaun.p.wassell@gmail.com>
2021-08-03 15:58:23 -0400

    07 - Added redux-persist

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // For localStorage
+// import storageSession from 'redux-persist/lib/storage/session'; // For session storage
+// import AsyncStorage from '@react-native-community/async-storage' // For React Native
 import { favoritesReducer } from './reducers/favorites';
 import { friendsReducer } from './reducers/friends';
 import { profileReducer } from './reducers/profile';
@@ -10,4 +14,12 @@ const rootReducer = combineReducers({
     profile: profileReducer,
 });
 
-export const store = createStore(rootReducer, devToolsEnhancer());
+const persistConfig = {
+  key: 'friendsTrackerRoot', // Because we might be developing several different apps on localhost:3000
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // Creates a sort of "wrapper" that updates localStorage when our store changes
+
+export const store = createStore(persistedReducer, devToolsEnhancer());
+export const persistor = persistStore(store)
