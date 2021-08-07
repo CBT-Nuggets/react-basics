871ef37645e5800afe6186af0bb4cabfe0e61860
Shaun Wassell <shaun.p.wassell@gmail.com>
2021-08-07 16:43:35 -0400

    Added new files for page tests, still no tests though

---
+++ src/test-utils/renderers.js
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { FavoritesContext } from '../contexts/FavoritesContext';
+import { FriendsContext } from '../contexts/FriendsContext';
+
+// You could do these separately, then combine them into one
+// export const renderWithFavorites = (elements, favoritesOptions, options) => render(
+//     <FavoritesContext.Provider value={favoritesOptions}>
+//         {elements}
+//     </FavoritesContext.Provider>,
+//     options,
+// );
+
+// export const renderWithFriends = (elements, friendsOptions, options) => render(
+//     <FriendsContext.Provider value={friendsOptions}>
+//         {elements}
+//     </FriendsContext.Provider>,
+//     options,
+// );
+
+export const renderWithResources = (elements, friendsOptions, favoritesOptions, options) => render(
+    <FriendsContext.Provider value={friendsOptions}>
+        <FavoritesContext.Provider value={favoritesOptions}>
+            {elements}
+        </FavoritesContext.Provider>,
+    </FriendsContext.Provider>,
+    options,
+);
